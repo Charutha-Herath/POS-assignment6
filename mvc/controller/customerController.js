b72173fefba09e4f875a18c9c22f5ebd127c4e39
Charutha-Herath <charuh2023@gmail.com>
2024-05-18 15:30:51 +0530

    done geneId

--- mvc/controller/customerController.js
+++ mvc/controller/customerController.js
@@ -1,5 +1,5 @@
 import {customer_db} from "../db/db.js";
-import {CustomerModel} from "../model/customerModel";
+import {CustomerModel} from "../model/customerModel.js";
 
 let submit = $('#Customer .btn-success').eq(0);
 let update = $('#Customer .btn-primary').eq(0);
@@ -19,10 +19,27 @@ let searchField=$('#searchField');
 
 
 
-$('#customer_page').on('click', function() {
+$('#nav-customer').on('click', function() {
     customer_id.val(generateCustomerId());
-    populateCustomerTable();
+    /*populateCustomerTable();
     delete_btn.prop("disabled", true);
     update.prop("disabled", true);
-    searchField.attr("placeholder", "Search Customer Here");
+    searchField.attr("placeholder", "Search Customer Here");*/
 });
+
+function generateCustomerId() {
+    let highestCustId = 0;
+
+    for (let i = 0; i < customer_db.length; i++) {
+        // Extract the numeric part of the item code
+        const numericPart = parseInt(customer_db[i].customer_id.split('-')[1]);
+
+        // Check if the numeric part is greater than the current highest
+        if (!isNaN(numericPart) && numericPart > highestCustId) {
+            highestCustId = numericPart;
+        }
+    }
+
+    // Increment the highest numeric part and format as "item-XXX"
+    return `cust-${String(highestCustId + 1).padStart(3, '0')}`;
+}
