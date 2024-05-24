2b33bbc04fb90cdf2b0c9ce28e596f99b5d66a23
Charutha-Herath <charuh2023@gmail.com>
2024-05-24 20:59:26 +0530

    update order details

--- mvc/controller/orderDetailsController.js
+++ mvc/controller/orderDetailsController.js
@@ -1,5 +1,8 @@
+import {customer_db, order_db} from "../db/db.js";
+import {OrderModel} from "../model/placeOrderModel.js";
 
-
+import {order_details_db} from "../db/db.js";
+import {orderModel} from "../model/orderDetailsModel.js";
 
 
 
