d827c6eb300b57647566d83af773cbb74649f79d
Charutha-Herath <charuh2023@gmail.com>
2024-05-24 18:03:03 +0530

    updated order add / table

--- mvc/controller/placeOrderController.js
+++ mvc/controller/placeOrderController.js
@@ -32,6 +32,8 @@ let removeBtn2=$('#order_delete_btn');
 let resetItemDetails=$('#order_reset_btn');
 
 
+let items = [];
+
 
 
 
@@ -46,12 +48,29 @@ $('#nav-place-order').on('click', function() {
     updateBtn2.prop("disabled",true);
     removeBtn2.prop("disabled",true);
     generateCurrentDate();
+    populateItemIDs();
     /*searchField.attr("placeholder", "Search Order Id Here");*/
 
 });
 function generateCurrentDate(){
     $("#order_date").val(new Date().toISOString().slice(0, 10));
 }
+
+
+function populateItemIDs() {
+
+    // Clear existing options except the default one
+    itemIdCB.find("option:not(:first-child)").remove();
+
+    // Iterate through the customerArray and add options to the select element
+    for (let i = 0; i < item_db.length; i++) {
+        itemIdCB.append($("<option>", {
+            value: item_db[i].item_code,
+            text: item_db[i].item_code
+        }));
+    }
+}
+
 
 add.on("click", function () {
     let itemCodeValue = itemIdCB.val();
@@ -68,8 +87,10 @@ add.on("click", function () {
             itemName: itemNameValue,
             priceValue: priceValue,
             qtyOnHand: qtyOnHandValue,
-            qtyValue: qtyValue
+            getQty: qtyValue
         });
+
+        console.log("Item array : ",items);
 
         /*Populate the Item table*/
         populateItemTable();
@@ -94,15 +115,18 @@ function showValidationError(title, text) {
 }
 
 function populateItemTable() {
-    $('tbody').eq(3).empty();
+    $('tbody').eq(0).empty();
+
+    console.log("Items x array : ",items);
+
     items.map((item) => {
-        $('tbody').eq(3).append(
+        $('tbody').eq(0).append(
             `<tr>
                 <th scope="row">${item.itemCode}</th>
                 <td>${item.itemName}</td>
                 <td>${item.priceValue}</td>
                 <td>${item.qtyOnHand}</td>
-                <td>${item.qtyValue}</td>
+                <td>${item.getQty}</td>
             </tr>`
         );
     });
@@ -122,8 +146,87 @@ resetItemDetails.on("click", function () {
     itemName.val('');
     price.val('');
     qtyOnHand.val('');
+
     updateBtn2.prop("disabled", true);
     removeBtn2.prop("disabled",true);
     add.prop("disabled", false);
 });
 
+itemIdCB.on("change", function() {
+    // Capture the selected value in a variable
+    let selectedValue = $(this).val();
+
+    let itemObj = $.grep(item_db, function(item) {
+        return item.item_code === selectedValue;
+    });
+
+    if (itemObj.length > 0) {
+        // Access the first element in the filtered array
+        itemName.val(itemObj[0].item_name); // Assuming there is an 'item_name' property
+        price.val(itemObj[0].price);
+        qtyOnHand.val(itemObj[0].qty_on_hand);
+    }
+
+    // Check if the item is already in the items array
+    let existingItem = items.find(item => item.itemCode === selectedValue);
+
+    if (existingItem) {
+        updateBtn2.prop("disabled", false);
+        removeBtn2.prop("disabled",false);
+        add.prop("disabled", true);
+        qty.val(existingItem.qtyValue);
+    }
+});
+
+$('#item-order-table').on('click', 'tbody tr', function() {
+
+    let itemCodeValue = $(this).find('th').text();
+    let itemNameValue = $(this).find('td:eq(0)').text();
+    let priceValue = $(this).find('td:eq(1)').text();
+    let qtyOnHandValue = $(this).find('td:eq(2)').text();
+    let qtyValue=$(this).find('td:eq(3)').text();
+
+    itemIdCB.val(itemCodeValue);
+    itemName.val(itemNameValue);
+    price.val(priceValue);
+    qtyOnHand.val(qtyOnHandValue);
+    qty.val(qtyValue);
+
+    updateBtn2.prop("disabled", false);
+    removeBtn2.prop("disabled",false);
+    add.prop("disabled", true);
+
+
+});
+
+/*
+resetBtn.on("click", function () {
+    // Reset the form fields to their initial state
+    generateCurrentDate();
+    populateCustomerIDs();
+    populateItemIDs();
+    orderId.val(generateOrderId());
+    $("#total").val('');       // Reset the total
+    $("#discount").val('');    // Reset the discount
+    $("#Cash").val('');        // Reset the cash input
+    customerName.val('');
+    itemName.val('');
+    price.val('');
+    qtyOnHand.val('');
+    total.val('');
+    discountInput.val('');
+    cashInput.val('');
+    subTotalInput.val('');
+    balanceInput.val('');
+
+    /!*Clear the items array*!/
+    items = [];
+
+    /!*Clear the item order table*!/
+    $("#item-order-table tbody").empty();
+
+    updateBtn.prop("disabled", true);
+    deleteBtn.prop("disabled", true);
+    submitBtn.prop("disabled",false);
+
+});*/
